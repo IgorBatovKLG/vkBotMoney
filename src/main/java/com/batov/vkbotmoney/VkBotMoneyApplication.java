2c39489a5ebc777268d4a0a326540d0201fc272e
Batov <84968728+IgorBatovKLG@users.noreply.github.com>
2023-09-26 10:32:42 +0300

    1

--- src/main/java/com/batov/vkbotmoney/VkBotMoneyApplication.java
+++ src/main/java/com/batov/vkbotmoney/VkBotMoneyApplication.java
@@ -1,5 +1,6 @@
 package com.batov.vkbotmoney;
 
+import com.batov.vkbotmoney.Service.BalanceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
